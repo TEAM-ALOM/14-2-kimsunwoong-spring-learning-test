c873a6681f048cbc30d653223ca425dd47b1cf5f
kolom1234 <kolom1357@gmail.com>
2024-10-01 23:21:53 +0900

    3주차 과제

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -14,7 +14,6 @@ public class QueryingDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    /*
     private final RowMapper<Customer> actorRowMapper = (resultSet, rowNum) -> {
         Customer customer = new Customer(
                 resultSet.getLong("id"),
@@ -23,24 +22,28 @@ public class QueryingDAO {
         );
         return customer;
     };
-    추후 rowMapper에 대해 학습해보고 이용해보기
-    */
+    //추후 rowMapper에 대해 학습해보고 이용해보기
+
 
     /**
      * public <T> T queryForObject(String sql, Class<T> requiredType)
      */
     public int count() {
         //TODO : customers 디비에 포함되어있는 row가 몇개인지 확인하는 기능 구현
-        return 0;
-    }
+        String sql = "SELECT COUNT(*) FROM customers"; // sql 명령, 전체 행 수 반환
+
+        return jdbcTemplate.queryForObject(sql, Integer.class); //queryForObject(String sql, Class<T> requiredType)
+    } // sql 커리 문자열과 타입을 인자로 전달
 
     /**
      * public <T> T queryForObject(String sql, Class<T> requiredType, @Nullable Object... args)
      */
     public String getLastName(Long id) {
         //TODO : 주어진 Id에 해당하는 customers의 lastName을 반환
-        return null;
-    }
+        String sql = "SELECT last_name FROM customers WHERE id = ?";
+        // ? 기호는 매개변수 바인딩을 위한 플레이스 홀더
+        return jdbcTemplate.queryForObject(sql, String.class, id);
+    } // 주어진 고객 ID를 기반으로 테이블에 해당 고객의 lastname 반환
 
     /**
      * public <T> T queryForObject(String sql, RowMapper<T> rowMapper, @Nullable Object... args)
@@ -48,7 +51,19 @@ public class QueryingDAO {
     public Customer findCustomerById(Long id) {
         String sql = "select id, first_name, last_name from customers where id = ?";
         //TODO : 주어진 Id에 해당하는 customer를 객체로 반환
-        return null;
+        return jdbcTemplate.queryForObject(sql, actorRowMapper, id);
+        /*
+        RowMapper는 SQL 쿼리 결과를 Java 객체로 변환하는데 사용됨.
+        이 경우 actorRowMapper는 ResultSet에서 데이터를 가져와 Customer 객체를 생성
+        (resultSet, rowNum) -> {
+                    Customer customer = new Customer(
+                            resultSet.getLong("id"),
+                            resultSet.getString("first_name"),
+                            resultSet.getString("last_name")
+                    );
+                    return customer;
+                }
+         */
     }
 
     /**
@@ -57,7 +72,7 @@ public class QueryingDAO {
     public List<Customer> findAllCustomers() {
         String sql = "select id, first_name, last_name from customers";
         //TODO : 저장된 모든 Customers를 list형태로 반환
-        return null;
+        return jdbcTemplate.query(sql, actorRowMapper);
     }
 
     /**
@@ -66,6 +81,6 @@ public class QueryingDAO {
     public List<Customer> findCustomerByFirstName(String firstName) {
         String sql = "select id, first_name, last_name from customers where first_name = ?";
         //TODO : firstName을 기준으로 customer를 list형태로 반환
-        return null;
+        return jdbcTemplate.query(sql, actorRowMapper, firstName);
     }
 }
--- spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
@@ -16,7 +16,7 @@ public class UpdatingDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    /*
+
     private final RowMapper<Customer> actorRowMapper = (resultSet, rowNum) -> {
         Customer customer = new Customer(
                 resultSet.getLong("id"),
@@ -25,21 +25,27 @@ public class UpdatingDAO {
         );
         return customer;
     };
-    추후 rowMapper에 대해 학습해보고 이용해보기
-    */
+    //추후 rowMapper에 대해 학습해보고 이용해보기
+
 
     /**
      * public int update(String sql, @Nullable Object... args)
      */
     public void insert(Customer customer) {
         //todo: customer를 디비에 저장하기
+        String sql = "INSERT INTO customers (first_name, last_name) VALUES (?, ?)";
+        jdbcTemplate.update(sql, customer.getFirstName(), customer.getLastName());
     }
+    // update 메소드는 데이터베이스에 INSERT, UPDATE, DELETE 쿼리를 실행할 때 사용됨
+    // ? 는 바인딩될 매개변수를 의미하고 customer.getFirstName()과 customer.getLastName()을 통해 Customer 객체의 이름과 성을 쿼리에 전달
     /**
      * public int update(String sql, @Nullable Object... args)
      */
     public int delete(Long id) {
         //todo: id에 해당하는 customer를 지우고, 해당 쿼리에 영향받는 row 수반환하기
-        return 0;
+        String sql = "DELETE FROM customers WHERE id = ?";
+        return jdbcTemplate.update(sql, id);
+        // 삭제된 행의 수 반환
     }
 
     /**
@@ -48,9 +54,21 @@ public class UpdatingDAO {
     public Long insertWithKeyHolder(Customer customer) {
         String sql = "insert into customers (first_name, last_name) values (?, ?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
+        //KeyHolder는 데이터베이스에서 생성된 자동 증가 키 값을 저장하는 데 사용됨
+        //GeneratedKeyHolder는 이 값을 저장하는 구현체
+        //PreparedStatement의 두 번째 인자로 new String[] {"id"}를 전달하여, 삽입 후 생성된 id 값을 가져오도록 설정
 
         //todo : keyHolder에 대해 학습하고, Customer를 저장후 저장된 Customer의 id를 반환하기
 
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[] {"id"});
+            ps.setString(1, customer.getFirstName());
+            ps.setString(2, customer.getLastName());
+            return ps;
+        }, keyHolder);
+        // PreparedStatementCreator를 통해 SQL 쿼리를 준비하고 실행하며, KeyHolder에 생성된 키 값을 저장
+
+        //  새로운 고객을 삽입할 때 해당 고객의 id 값 가져옴
         Long id = keyHolder.getKey().longValue();
 
         return keyHolder.getKey().longValue();
