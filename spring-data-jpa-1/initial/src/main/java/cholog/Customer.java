a5c029a3efeb32886a61dbd04460fbfbb3d9c739
kolom1234 <kolom1357@gmail.com>
2024-10-08 21:50:29 +0900

    4주차 과제

--- spring-data-jpa-1/initial/src/main/java/cholog/Customer.java
+++ spring-data-jpa-1/initial/src/main/java/cholog/Customer.java
@@ -1,7 +1,17 @@
 package cholog;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // customer 클래스가 jpa엔티티임
 public class Customer {
 
+    @Id // 기본 키로 사용될 필드 지정
+    // 기본 키가 데이터베이스에서 자동으로 생성되는 것을 나타냄, IDENTITY
+    // 전략을 통해 데이터베이스에서 자동 증가된 값을 할당 받음
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
